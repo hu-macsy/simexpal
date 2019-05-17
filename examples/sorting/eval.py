6f8ce6690585da913a224476f0e4b9b55e26077b
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-05-17 15:52:03 +0200

    examples/sorting: Add evaluation script

---
+++ examples/sorting/eval.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import simexpal
+import pandas
+import yaml
+
+def parse(run, f):
+	output = yaml.load(f, Loader=yaml.Loader)
+	return {
+		'experiment': run.experiment.name,
+		'instance': run.instance.shortname,
+		'time': output['time']
+	}
+
+cfg = simexpal.config_for_dir()
+df = pandas.DataFrame(cfg.collect_successful_results(parse))
+print(df.groupby('experiment').agg({'time': 'mean'}))
