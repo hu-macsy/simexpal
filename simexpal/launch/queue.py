cc127fe1fb42a0bd04fe0ff8a12370d60565f035
Duy Le Thanh <lethanhd@informatik.hu-berlin.de>
2020-11-20 19:49:30 +0100

    Overhaul QueueLauncher (fix issue #77)

--- simexpal/launch/queue.py
+++ simexpal/launch/queue.py
@@ -1,18 +1,32 @@
 
+import os
+import tempfile
+
 from . import common
 from .. import queuesock
+from .. import util
 
 class QueueLauncher(common.Launcher):
-	def submit(self, config, run):
+	def submit(self, cfg, run):
+		util.try_mkdir(os.path.join(cfg.basedir, 'aux'))
+		util.try_mkdir(os.path.join(cfg.basedir, 'aux/_queue'))
+
 		if not common.lock_run(run):
 			return
 		common.create_run_file(run)
+
+		specfd, specfile = tempfile.mkstemp(prefix='', suffix='-spec.yml',
+											dir=os.path.join(cfg.basedir, 'aux/_queue'))
+
+		specs = {'manifest': common.compile_manifest(run).yml}
+		with os.fdopen(specfd, 'w') as f:
+			util.write_yaml_file(f, specs)
 
 		print("Launching experiment '{}', instance '{}' on local machine".format(
 				run.experiment.name, run.instance.shortname))
 
 		queuesock.sendrecv({
 			'action': 'launch',
-			'manifest': common.compile_manifest(run).yml
+			'specfile_path': specfile
 		})
 
--- simexpal/queuesock.py
+++ simexpal/queuesock.py
@@ -1,105 +1,120 @@
 
-from enum import Enum
-import functools
 import os
 import socket
+import selectors
+import subprocess
+import sys
+import tempfile
 
-from . import launch
-from .launch import common
 from . import util
-from . import evloop
 
-class _State(Enum):
-	REQUEST = 0
-	DONE = 1
 
-class _Queue:
-	@staticmethod
-	def handle_sock(self, descriptor):
-		accepted, _ = self.sock.accept()
-		con = _Connection(self, accepted)
-		con.run(descriptor.get_loop())
+class Queue:
+
+	def __init__(self, serve_socket):
+		self.socket = serve_socket
+		self.socket_path = serve_socket.getsockname()
+		self.selector = selectors.DefaultSelector()
+		self._should_stop = False
 
 	@staticmethod
-	def observe_loop(self, descriptor):
-		self._handle.unregister()
-		self._observer.unregister()
+	def get_display_name(manifest):
+		display_name = manifest['experiment']
 
-		if self.path is not None:
-			os.unlink(self.path)
+		variants = manifest['variants']
+		if variants:
+			display_name += ' ~ ' + ', '.join([variant['name'] for variant in variants])
 
-	def __init__(self, sock, path=None):
-		self.sock = sock
-		self.path = path
-		self._handle = None
-		self._observer = None
+		revision = manifest['revision']
+		if revision:
+			display_name += ' @ ' + revision
 
-	def run(self, loop):
-		self._handle = loop.register_file(self.sock, evloop.READ,
-				functools.partial(_Queue.handle_sock, self))
-		self._observer = loop.register_observer(functools.partial(_Queue.observe_loop, self))
-		self.sock.listen()
+		return display_name
 
-	# Dispatch a request and return a response.
-	# This function is the main workhorse of the queue server.
-	def dispatch(self, loop, req):
-		if req['action'] == 'stop':
-			loop.shutdown()
-		else:
-			assert req['action'] == 'launch'
-			manifest = launch.common.RunManifest(req['manifest'])
+	def run(self):
+		print('Serving on {}'.format(self.socket_path))
+		self.socket.listen()
+		self.selector.register(self.socket, selectors.EVENT_READ)
 
-			print("Processing experiment '{}', instance '{}'".format(
-					manifest.experiment, manifest.instance))
-			launch.common.invoke_run(manifest)
+		requests = []
+		cur_subprocess = None
+		subprocess_terminated = True
+		while True:
+			events = self.selector.select(1)
 
-class _Connection:
-	@staticmethod
-	def handle_sock(self, descriptor):
-		assert self.state == _State.REQUEST
+			for sk, mask in events:
+				if sk.fileobj == self.socket:
+					conn, _ = self.socket.accept()
+					connection = Connection(conn)
 
-		data = self.sock.recv(4096)
-		if data:
-			self.recv_buffer += data
-			return
+					self.selector.register(conn, selectors.EVENT_READ, connection)
+				else:
+					request = sk.data.progress()
+					if request:
+						if request['action'] == 'launch':
+							requests.append(request)
+						elif request['action'] == 'stop':
+							self._should_stop = True
+						else:
+							print("Ignoring request with unknown action '{}': {}".format(request['action'], request))
 
-		req = util.yaml_from_string(self.recv_buffer.decode())
-		resp = self.queue.dispatch(descriptor.get_loop(), req)
-		self.state = _State.DONE
-		self._handle.unregister()
+					sk.data.close()
+					self.selector.unregister(sk.fd)
 
-	@staticmethod
-	def observe_loop(self, descriptor):
-		self._handle.unregister()
-		self._observer.unregister()
+			if cur_subprocess is not None:
+				subprocess_terminated = cur_subprocess.poll() is not None
 
-	def __init__(self, queue, sock):
-		self.queue = queue
-		self.sock = sock
-		self.recv_buffer = bytes()
-		self.state = _State.REQUEST
-		self._handle = None
-		self._observer = None
+			if subprocess_terminated and not len(requests) == 0:
+				request = requests.pop(0)
 
-	def run(self, loop):
-		self._handle = loop.register_file(self.sock, evloop.READ,
-				functools.partial(_Connection.handle_sock, self))
-		self._observer = loop.register_observer(functools.partial(_Connection.observe_loop, self))
+				if self._should_stop:
+					print("Closing socket on {}".format(self.socket_path))
+					self.socket.close()
+					os.remove(self.socket_path)
+					break
 
-def run_queue(loop, sockfd=None, force=False):
+				if request['action'] == 'launch':
+					specfile_path = request['specfile_path']
+					with open(specfile_path, 'r') as f:
+						manifest = util.read_yaml_file(f)['manifest']
+
+					display_name = self.get_display_name(manifest)
+					print("Launching run {}/{}[{}]".format(
+						display_name, manifest['instance'], manifest['repetition']))
+
+					script = os.path.abspath(sys.argv[0])
+
+					cur_subprocess = subprocess.Popen([script, 'internal-invoke', '--method=queue', specfile_path])
+
+class Connection:
+	def __init__(self, connection):
+		self.connection = connection
+
+	def progress(self):
+		recv_buffer = bytes()
+		while True:
+			data = self.connection.recv(4096)
+			if not data:
+				break
+			recv_buffer += data
+
+		return util.yaml_from_string(recv_buffer.decode())
+
+	def close(self):
+		self.connection.close()
+
+def run_queue(sockfd=None, force=False):
 	if sockfd is not None:
 		serve_sock = socket.socket(fileno=sockfd)
-		queue = _Queue(serve_sock)
 	else:
 		sockpath = os.path.expanduser('~/.extlq.sock')
 		serve_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		if force:
 			util.try_rmfile(sockpath)
 		serve_sock.bind(sockpath)
-		queue = _Queue(serve_sock, path=sockpath)
 
-	queue.run(loop)
-	print('Serving on {}'.format(serve_sock.getsockname()))
+	queue = Queue(serve_sock)
+	queue.run()
 
 def sendrecv(m):
 	sockpath = os.path.expanduser('~/.extlq.sock')
