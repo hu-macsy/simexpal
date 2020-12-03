656bcc36ce696076f20153de6de70617ddc23622
Duy Le Thanh <lethanhd@informatik.hu-berlin.de>
2020-12-03 18:42:04 +0100

    Add Github Actions workflow and unit tests for 'util.validate_setup_file()' and cli

---
+++ tests/validation/test_experiments_yml.py
@@ -0,0 +1,27 @@
+
+import os
+import pytest
+
+from simexpal import util
+
+file_dir = os.path.abspath(os.path.dirname(__file__))
+valid_experiments_ymls = ['/../../examples/sorting/experiments.yml',
+                          '/../../examples/sorting_cpp/experiments.yml',
+                          '/../../examples/download_instances/experiments.yml']
+
+invalid_experiments_ymls = ['/invalid_ymls/top_level-additional_property.yml',
+                            '/invalid_ymls/repo-invalid_value.yml']
+
+@pytest.mark.parametrize('rel_yml_path', valid_experiments_ymls)
+def test_valid_experiments_yml(rel_yml_path):
+    ret_val = util.validate_setup_file(file_dir + rel_yml_path)
+
+    assert isinstance(ret_val, dict)
+    assert 'instdir' in ret_val
+
+@pytest.mark.parametrize('rel_yml_path', invalid_experiments_ymls)
+def test_invalid_experiments_yml(rel_yml_path):
+    with pytest.raises(SystemExit) as info:
+        util.validate_setup_file(file_dir + rel_yml_path)
+
+    assert info.value.code == 1
