9d56f9cbef7c7e054f9c846ad60b5ebfed4f0541
Jones <2944106+c-bebop@users.noreply.github.com>
2024-01-05 14:30:14 +0100

    Initial setup of fileless instance sorting experiment.

---
+++ examples/sorting_fileless/eval.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import simexpal
+import pandas
+import yaml
+
+def parse(run, f):
+	output = yaml.load(f, Loader=yaml.Loader)
+	return {
+		'experiment': run.experiment.name,
+		'instance': run.instance.shortname,
+		'comparisons': output['comparisons'],
+		'swaps': output['swaps'],
+		'time': output['time']
+	}
+
+cfg = simexpal.config_for_dir()
+results = []
+for successful_run in cfg.collect_successful_results():
+	with successful_run.open_output_file() as f:
+		results.append(parse(successful_run, f))
+
+df = pandas.DataFrame(results)
+print(df.groupby('experiment')[['comparisons', 'swaps', 'time']].agg('mean'))
