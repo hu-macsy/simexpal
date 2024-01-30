27e791ced6374b75166768efcb3e893221c011ec
nizomovs <nizomovshakhriyor@gmail.com>
2024-01-30 21:35:28 +0100

    Fix comment

--- tests/instances/test_subdirs.py
+++ tests/instances/test_subdirs.py
@@ -8,6 +8,6 @@ file_dir = os.path.abspath(os.path.dirname(__file__))
 def test_experiments_missing_program():
     cfg = base.config_for_dir(file_dir + '/experiments_ymls/subdir/')
 
-    # Make sure that the needed instances are installed.
+    # Make sure that all the instances are available.
     for instance in cfg.all_instances():
         assert instance.check_available()
