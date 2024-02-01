23e538d400d9fd7d741ac7ebbed687c464c6bc09
nizomovs <nizomovshakhriyor@gmail.com>
2024-02-01 12:06:36 +0100

    Remove old comment from test_builds.py.

--- tests/builds/test_builds.py
+++ tests/builds/test_builds.py
@@ -12,7 +12,6 @@ def test_simex_d_vcs_less():
     revision = cfg.get_revision('main')
     vcs_less_build = cfg.get_build('vcs-less', revision)
 
-    # Passing None for args is fine since it's properties are not used for vcs-less builds
     build.make_builds(cfg, revision, [vcs_less_build.info], ['vcs-less'], [build.Phase.INSTALL])
 
     def check_files_in_dir(files, dir):
