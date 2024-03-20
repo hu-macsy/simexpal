0e9aa9bf8c659a08a5e2707505676dabfd4d67c8
nizomovs <nizomovshakhriyor@gmail.com>
2024-03-20 16:14:59 +0100

    Explicitly check the output of the valid experiment.

--- tests/instances/test_fileless.py
+++ tests/instances/test_fileless.py
@@ -8,14 +8,14 @@ from simexpal.launch import common
 
 file_dir = os.path.abspath(os.path.dirname(__file__))
 
-valid_experiments_ymls = ['experiments_ymls/fileless/instance_filename.yml'
+valid_experiments_ymls = [('experiments_ymls/fileless/instance_filename.yml', ['fileless-instance'])
                           ]
 
 invalid_experiments_ymls = [('experiments_ymls/fileless/instance.yml', "The instance 'fileless-instance' is fileless. Did you forget to remove the @INSTANCE@ variable in the argument list of the experiment?")
                             ]
 
-@pytest.mark.parametrize('rel_yml_path', valid_experiments_ymls)
-def test_valid_fileless_experiments_yml(rel_yml_path):
+@pytest.mark.parametrize('rel_yml_path, expected_output', valid_experiments_ymls)
+def test_valid_fileless_experiments_yml(rel_yml_path, expected_output):
     rel_yml_dir = os.path.dirname(rel_yml_path)
     yml_dir = os.path.join(file_dir, rel_yml_dir)
     filename = os.path.basename(rel_yml_path)
@@ -31,7 +31,9 @@ def test_valid_fileless_experiments_yml(rel_yml_path):
         manifest = common.compile_manifest(run)
         common.invoke_run(manifest)
 
-        os.path.getsize(run.output_file_path('out'))
+        with open(run.output_file_path('out'), 'r') as output:
+            actual_output = output.read().splitlines()
+            assert expected_output == actual_output
 
 @pytest.mark.parametrize('rel_yml_path, error_message', invalid_experiments_ymls)
 def test_invalid_fileless_experiments_yml(rel_yml_path, error_message):
