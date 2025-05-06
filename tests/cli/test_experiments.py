291641edfbbbd5a18ea930426c3b0b67b60c5e20
Lukas Berner <Lukas.Berner@hu-berlin.de>
2025-05-06 12:04:30 +0200

    CI: Modernize (add slurm testing, remove EOL Python) (#184)
    * add slurm testing to ci on ubuntu
    * split tests ubuntu/mac
    * remove EOL python 3.8
    * add python 3.13
    * cleanup pip install commands
    
    ---------
    
    Co-authored-by: fabratu <brandtfa@hu-berlin.de>

--- tests/cli/test_experiments.py
+++ tests/cli/test_experiments.py
@@ -1,4 +1,3 @@
-
 import os
 import pytest
 import subprocess
@@ -6,19 +5,24 @@ import subprocess
 file_dir = os.path.abspath(os.path.dirname(__file__))
 yml_dirs = ['/../../examples/sorting/']  # List of directories that should be tested.
 
-@pytest.mark.parametrize('rel_yml_path', yml_dirs)
-def test_simex_e_launch_fork(rel_yml_path):
+
+@pytest.mark.parametrize("launcher", ["fork", "slurm"])
+@pytest.mark.parametrize("rel_yml_path", yml_dirs)
+def test_simex_e_launch(launcher, rel_yml_path):
     # Make sure that the needed instances are installed.
     # The main test follows afterwards.
     cwd = file_dir + rel_yml_path
-    ret_code = subprocess.check_call(['simex', 'i', 'install'], cwd=cwd)
+    ret_code = subprocess.check_call(["simex", "i", "install"], cwd=cwd)
     assert ret_code == 0
 
-    ret_code = subprocess.check_call(['simex', 'e', 'launch', '--launch-through=fork'], cwd=cwd)
+    ret_code = subprocess.check_call(
+        ["simex", "e", "launch", f"--launch-through={launcher}"], cwd=cwd
+    )
 
     assert ret_code == 0
 
-@pytest.mark.parametrize('rel_yml_path', yml_dirs)
+
+@pytest.mark.parametrize("rel_yml_path", yml_dirs)
 def test_simex_e_purge_all(rel_yml_path):
     cwd = file_dir + rel_yml_path
     ret_code = subprocess.check_call(['simex', 'e', 'purge', '--all', '-f'], cwd=cwd)
